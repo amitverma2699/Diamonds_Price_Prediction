94a51a888a568363b43af77797e99ae460f08b07
amitverma2699 <amit99bhama@gmail.com>
2024-09-08 12:51:25 +0530

    Update File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+setup(
+    name='DiamondsPricePrediction',
+    version='0.0.1',
+    author='Amit Verma',
+    author_email='amit99bhama@gmail.com',
+    #install_requires=["scikit-learn","pandas","numpy"],
+    packages=find_packages()
+)
